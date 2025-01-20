eaa398f48adf0b7d64d079622079150ac88ea15a
paneer24 <99405385+paneer24@users.noreply.github.com>
2025-01-20 09:03:26 +0530

    Create Temperateure_Conveter.py

---
+++ Coding Challenge/Temperateure_Conveter.py
@@ -0,0 +1,7 @@
+# To convert fahrenheit  to celsius and celesius to fahrenheit 
+C=float(input("Enter the value for the temperature for Celsius"))
+F=float(input("Enter the value for the temperature for Fahreneheit"))
+C=(5/9)*(F-32)
+print("The value of the Fahreneit in Celsius is",C)
+F=(9/5)*C+32
+print("The value of Celsius in Fahreneit is",F)
